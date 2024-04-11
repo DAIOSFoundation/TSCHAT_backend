49e0d5cc25fb071c302883379ce5097e0ab39736
ksm <ksm@daiblab.com>
2024-04-11 10:52:21 +0900

    get

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MessageView
+from .views import MessageView,GetUnreadMSG
 
 urlpatterns = [
     path('message/', MessageView.as_view(), name='message'),
+    path('GetUnreadMSG/', GetUnreadMSG.as_view(), name='GetUnreadMSG'),
 ]
--- API/views.py
+++ API/views.py
@@ -16,3 +16,21 @@ class MessageView(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response({"detail": "Invalid API key."}, status=status.HTTP_403_FORBIDDEN)
+    
+    
+    
+
+
+class GetUnreadMSG(APIView):
+    def post(self, request):
+        key = request.data.get('key')
+        if key == 'abcdef': #db에서 user 키 발급
+            user_id = request.data.get('data', {}).get('userid')
+            if user_id:
+                messages = Message.objects.filter(userid=user_id)
+                serializer = MessageSerializer(messages, many=True)
+                return Response(serializer.data)
+            else:
+                return Response({'error': 'Missing or invalid userid'}, status=400)
+            
+        return Response({'error': 'Missing or invalid userid'}, status=400)
